831325323b1f9126f24d6bed8f10cd36ef0dfd2c
hcustod <111096105+hcustod@users.noreply.github.com>
2023-01-09 06:11:09 -0500

    first

---
+++ my-portfolio/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a className="navbar-brand" href="/">
+        My Portfolio
+      </a>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" href="/about">
+            About
+          </a>
+        </li>
+        <p> HELLLLLOOOOOO </p>
+        <li className="nav-item">
+          <a className="nav-link" href="/projects">
+            Projects
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
